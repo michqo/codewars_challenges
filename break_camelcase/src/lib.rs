06a26d7f1a607d2ce75122f20b8f276501dbd652
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2021-10-02 17:28:21 +0200

    Break camelCase unsolved

---
+++ break_camelcase/src/lib.rs
@@ -0,0 +1,14 @@
+fn solution(s: &str) -> String {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solution() {
+        assert_eq!(solution("camelCasing"), "camel Casing");
+        assert_eq!(solution("camelCasingTest"), "camel Casing Test");
+    }
+}
